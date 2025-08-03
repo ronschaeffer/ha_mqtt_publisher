6f8927d07dc3ebebbdf18fef12551c9b60274679
ronschaeffer <ron@ronschaeffer.com>
2025-08-03 22:32:37 +0100

    Update author information to be consistent
    - Set author to 'ronschaeffer' and email to 'ron@ronschaeffer.com'
    - Add version and author info to __init__.py
    - Matches other projects in the workspace

--- mqtt_publisher/__init__.py
+++ mqtt_publisher/__init__.py
@@ -1,1 +1,5 @@
-# This file can be empty or contain package initialization code
+"""An MQTT publisher package"""
+
+__version__ = "0.1.0"
+__author__ = "ronschaeffer"
+__email__ = "ron@ronschaeffer.com"
