310313048965aafee830fdcb22ae0117bb957b4b
ronschaeffer <ron@ronschaeffer.com>
2025-08-11 15:34:48 +0100

    chore: bump version to 0.2.4

--- src/ha_mqtt_publisher/__init__.py
+++ src/ha_mqtt_publisher/__init__.py
@@ -1,6 +1,6 @@
 """An MQTT publisher package with Home Assistant Discovery support"""
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = "ronschaeffer"
 
 # Import main components for easy access
